37e29f9b444a2b67a50923fc168a08caefd2e0a0
k.hino <k.hino.sys22@morijyobi.ac.jp>
2022-12-05 11:38:30 +0900

    Main.javaを2人分出力に変更

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        for(int i=0;i<1;i++){
+        for(int i=0;i<2;i++){
                 System.out.print("身長");
             double height = s.nextDouble();
             System.out.print("体重");
